bd55b43633045b464eeded148966792b6eead95e
marcelina13k <mkrowinska@gmail.com>
2025-03-03 22:45:34 -0500

    enabling site analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import { Providers } from "./providers"
 import { Toaster } from "@/components/ui/toaster"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Providers>
           {children}
+          <Analytics />
           <Toaster />
         </Providers>
       </body>
